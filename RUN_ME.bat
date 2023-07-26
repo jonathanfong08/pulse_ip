72d990f9bde18e5c6be017b329df24815770efa7
jonathanfong08 <122180696+jonathanfong08@users.noreply.github.com>
2023-07-26 15:21:48 -0500

    Add files via upload

---
+++ RUN_ME.bat
@@ -0,0 +1,18 @@
+@echo off
+title Installing Modules For Pulse
+cls
+py -m pip install requests
+py -m pip install pystyle
+py -m pip install json
+py -m pip install os
+py -m pip install getpass
+py -m pip install pyperclip
+py -m pip install time
+py -m pip install webbrowser
+py -m pip install colorama
+py -m pip install ctypes
+py -m pip install discord_webhook
+cls
+title Everything is installed.
+echo Created by jonnywick!
+pause
